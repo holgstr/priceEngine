8b158c7531b9341c2c3b2c496a741c96d71b4069
Holger Löwe <h.loewe@campus.lmu.de>
2023-10-01 19:22:05 +0200

    add regret function

--- R/demand_functions.R
+++ R/demand_functions.R
@@ -6,30 +6,36 @@ linear_revenue <- function(a, b, p) {
   p * linear_demand(a, b, p)
 }
 
-logistic_demand <- function(a, b, p, beta, X) {
+logistic_demand <- function(a, b, p, beta = NULL, X = NULL) {
   assert_named(beta)
+  assert_named(X)
 
   lin_pred <- b * p + sum(beta * X)
   a * (1 / (1 + exp(-lin_pred)))
 }
 
-logistic_revenue <- function(a, b, p, beta, X) {
+logistic_revenue <- function(a, b, p, beta = NULL, X = NULL) {
   p * logistic_demand(a, b, p, beta, X)
 }
 
-optimizand_neg <- function(fun, ...) {
-  -fun(...)
+regret_revenue <- function(revenue, delta = -0.2, interval = c(0, 1000), ...) {
+  oracle <- optimize(
+    f = revenue,
+    interval = interval,
+    maximum = TRUE,
+    ...
+  )
+  p_oracle <- oracle$maximum
+  rev_oracle <- oracle$objective
+  # Compute regret:
+  p_actual <- p_oracle * (1 + delta)
+  abs_regret <- rev_oracle - revenue(p_actual, ...)
+  rel_regret <- abs_regret / rev_oracle
+  list(
+    "actual_price" = p_actual,
+    "oracle_price" = p_oracle,
+    "oracle_revenue" = rev_oracle,
+    "abs_regret" = abs_regret,
+    "rel_regret" = rel_regret
+  )
 }
-
-optimize(
-  f = optimizand_neg,
-  interval = c(.Machine$double.eps, 1000),
-  fun = logistic_revenue,
-  a = 1000,
-  b = -0.02,
-  beta = c("beta1" = -0.2),
-  X = c("x1" = 4)
-)
-
-# add function revenue_regret, that computes what proportion of revenue is lost
-# due to non-optimal price specification of a certain parametrization and price
